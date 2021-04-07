fe336aeaa096a703d2f38cacd70d4f7849ee0c57
Areum Kim <kimarum2003@gmail.com>
2021-04-08 08:59:48 +0900

    버튼 추가하고 클릭 이벤트 핸들러 구현

--- HelloWinform/Form1.Designer.cs
+++ HelloWinform/Form1.Designer.cs
@@ -29,21 +29,34 @@ namespace HelloWinform
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 140);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "클릭!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(550, 313);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
